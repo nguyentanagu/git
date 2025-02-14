8107e9ad46b28fb75d93f8f9f77bc35747971b74
Nguyen Nhat Tan <tan@gmail.com>
2025-02-14 16:54:03 +0000

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from "./shared/header-layout/header-layout.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderLayoutComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  //text
+  title = {
+    name:'NguyenTan',
+    old:'22',
+  };
+  //properties
+  isDisable=false;
+}
